c94b6eeccbe86d3b0d6e23b78870e273ad5f5519
Olise <you@example.abanum00@gmail.com>
2022-06-28 18:37:52 -0700

    conditionals and formatting

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if (number % 10) > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, number % 10))
+elif (number % 10) < 5:                                         
+    print("Last digit of {} is {} and is less than 5".format(number. number % 10))
+else:
+    print("Last digit of {} is {} and is0".format(numner, number % 10))
+
+
+
+
